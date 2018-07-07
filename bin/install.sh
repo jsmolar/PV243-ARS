53ffce1ef906716ee6b2d06eafeeac1cca09cfa7
Jakub Smolar <jsmolar@redhat.com>
2018-07-07 20:54:34 +0200

    Profile for OC

---
+++ bin/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+unzip -o /tmp/src/config/keycloak-wildfly-adapter-dist-4.1.0.Final.zip -d ${JBOSS_HOME}
+cd ${JBOSS_HOME}
+./bin/jboss-cli.sh --file=bin/adapter-install-offline.cli
+chmod -R g+w ./
