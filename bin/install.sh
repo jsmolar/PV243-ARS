e94794bd3e234a19d905b0ff242ccf51361c12b6
Martina Minátová <m.minatova@gmail.com>
2018-07-10 20:57:30 +0200

    jms configuration

--- bin/install.sh
+++ bin/install.sh
@@ -3,3 +3,4 @@
 #unzip /tmp/src/config/keycloak-wildfly-adapter-dist-4.1.0.Final.zip -d ${JBOSS_HOME}
 cd ${JBOSS_HOME}
 ./bin/jboss-cli.sh --file=bin/adapter-install-offline.cli
+./bin/jboss-cli.sh --file=bin/configure-jms.cli
